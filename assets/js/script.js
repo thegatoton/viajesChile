eb9b35694c22b6ab400032ac3b193a927b175d62
Alexis B <brionesbadembach@gmail.com>
2022-06-02 17:54:39 -0400

    added script for navbar

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(function()
+{
+    $('[data-bs-toggle="tooltip"]')
+}
+)
+
+$(document).scroll(function()
+{
+    const y = $("html").scrollTop();
+    y >300 ? $('nav').addClass("nav-black") : $("nav").removeClass("nav-black")
+})
